e0c59edde99beddfa3d00dfc3804c67805b6c3b6
Creata21 <yerezhepabdulla@gmail.com>
2023-03-22 18:50:55 +0600

    other

--- replica.js
+++ replica.js
@@ -14,9 +14,3 @@ function replica(...obj) {
     });
     return res
 }
-
-console.log(replica({},
-    { a: { b1: 1, c1: 2 } },
-    { a: { b1: { d2: 1, e2: 2 } } },
-    { a: { b1: { d2: { f3: 1, h3: 1 }, e2: { g3: 2 } } } },
-    { a: { b1: { d2: { f3: { i4: 1 }, h3: 1 }, e2: { g3: 2 } } } }))
