5b53b09d3736d01146f896837defa13dbd31abff
Creata21 <yerezhepabdulla@gmail.com>
2023-03-22 16:17:57 +0600

    func rename to invert

--- invert.js
+++ invert.js
@@ -1,7 +1,9 @@
 const invert = (o) => {
     let newO = {}
-    for (key in o) {
+    Object.keys(o).forEach(key => {
         newO[o[key]] = key
-    }
+        console.log(key)
+    });
     return newO
 }
+invert({a:2, b:3, c:4})
